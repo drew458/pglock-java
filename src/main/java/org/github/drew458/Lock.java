9905f750ccd9f44ed9883d843f7a3eb96368338b
drew458 <43808240+drew458@users.noreply.github.com>
2024-06-17 19:22:34 +0200

    add lock acquire

---
+++ src/main/java/org/github/drew458/Lock.java
@@ -0,0 +1,22 @@
+package org.github.drew458;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Lock {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    /**
+     * Obtains an exclusive transaction-level advisory lock if available.
+     * Does not wait if the lock cannot be acquired immediately
+     * @param lockCode A 64-bit integer
+     * @return true if it can obtain the lock immediately, or false if the lock cannot be acquired.
+     */
+    public Boolean acquireLock(Integer lockCode) {
+        return jdbcTemplate.queryForObject("SELECT pg_try_advisory_xact_lock(?)", Boolean.class, lockCode);
+    }
+}
